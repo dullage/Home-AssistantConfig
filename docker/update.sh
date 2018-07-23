81ba0125f65f53d119728ef6647c3edc97bb7caf
Adam Dullage <spam@dullage.com>
2018-07-23 10:44:24 +0100

    DuckDNS > CloudFlare

--- docker/update.sh
+++ docker/update.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# Note: This relies on the dockerfile being updated with the latest version number (as there doesn't seem to be a latest tag for the HA RPI container). It will also leave behind the old HA RPI image. Change Directory
-
-cd /home/pi/homeassistant/docker
+# Note: This relies on the dockerfile being updated with the latest version number (as there doesn't seem to be a latest tag for the HA RPI container). It will also leave behind the old HA RPI image. 
 
 # Stop the existing container
 docker stop homeassistant
@@ -12,6 +10,9 @@ docker rm homeassistant
 
 # Delete the existing image
 docker rmi myhomeassistant:latest
+
+# Change Directory
+cd /home/pi/homeassistant/docker
 
 # Run the docker compose file
 docker-compose up -d
