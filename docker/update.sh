14cfaef50bce9e06041158a614f73927874f2a4d
Adam Dullage <spam@dullage.com>
2018-07-31 21:32:24 +0100

    New Update Scripts

--- docker/update.sh
+++ docker/update.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 
-# Note: This relies on the dockerfile being updated with the latest version number (as there doesn't seem to be a latest tag for the HA RPI container). It will also leave behind the old HA RPI image. 
+# Note: This script will leave behind old HA RPI images. 
 
+# Ask for target version
+read -p "What version of Home Assistant would you like to install? " version
+printf "\n"
+
+# Pull the image
+docker pull homeassistant/raspberrypi3-homeassistant:$version || exit 1
+
+# Create a new docker-compose file from the docker-compose-template file using the target version number
+version_token="{{ version }}"
+
+sed -e "s/${version_token}/${version}/g" \
+    < docker-compose-template.yaml \
+    > docker-compose.yaml || exit 1
+
+printf "Disabling Hass Watcher..."
+wget -qO- https://reactor.dullage.com/timedjob/Hass%20Watcher/disable
+printf "\n"
+	
 # Stop the existing container
 docker stop homeassistant
 
 # Delete the existing container
 docker rm homeassistant
 
-# Delete the existing image
-docker rmi myhomeassistant:latest
-
-# Change Directory
-cd /home/pi/homeassistant/docker
-
 # Run the docker compose file
 docker-compose up -d
+
+printf "Waiting for 80 seconds for Homassistant to Boot...\n"
+sleep 80
+
+printf "Re-Enabling Hass Watcher..."
+wget -qO- https://reactor.dullage.com/timedjob/Hass%20Watcher/enable
+printf "\n"
