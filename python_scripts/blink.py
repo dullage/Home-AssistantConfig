f8db845a38536846bc9a1e88bd4c6420ea5c8b19
Adam Dullage <spam@dullage.com>
2018-11-10 14:59:08 +0000

    Various Changes

--- python_scripts/blink.py
+++ python_scripts/blink.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from time import sleep
 from yaml import load as yaml_load, YAMLError
+from os import system
 
 ########### USER VARIABLES ###########
 
@@ -39,14 +40,14 @@ with open("{0}/secrets.yaml".format(secretsFileLocation), 'r') as secretsFile:
 endpoint = "https://rest.prod.immedia-semi.com/login"
 
 headers = {
-	"Host":"prod.immedia-semi.com",
-	"Content-Type":"application/json"
+    "Host":"prod.immedia-semi.com",
+    "Content-Type":"application/json"
 }
 
 data = {
-	"email":secret["blinkUsername"],
-	"password":secret["blinkPassword"], 
-	"client_specifier":"iPhone 9.2 | 2.2 | 222"
+    "email":secret["blinkUsername"],
+    "password":secret["blinkPassword"], 
+    "client_specifier":"iPhone 9.2 | 2.2 | 222"
 }
 
 response = post(endpoint, headers = headers, data = json_dumps(data))
@@ -57,75 +58,78 @@ start = datetime.today()
 
 # Keep refreshing the video list until we have a video from the last minute.
 while True:
-	# Get a list of videos, assume page 0 has the most recent. Convert the JSON response to a dict.
-	responseData = get("https://rest-prde.immedia-semi.com/api/v2/videos/page/0", headers = {"Host":"prod.immedia-semi.com", "TOKEN_AUTH":token}).json()
-	
-	# Loop through all the videos to find the most recent
-	latestVideoTimestamp = datetime.utcfromtimestamp(0)
-	for video in responseData:
-		videoTimestamp = datetime.strptime(video["created_at"], "%Y-%m-%dT%H:%M:%S+00:00") + relativedelta(hours = timeOffsetHours)
-		
-		if videoTimestamp > latestVideoTimestamp:
-			latestVideoTimestamp = videoTimestamp
-			latestVideoUrl = video["address"]
-	
-	# Check to see if it's recent.
-	if latestVideoTimestamp > (start - relativedelta(minutes = goBackMinutes)):
-		# It's recent, continue.
-		elapsedTime = abs(datetime.today() - start).total_seconds()
-		break
-	else:
-		# It's old.
-		# If we've been looping longer than the waitTimeoutSeconds, break.
-		if datetime.today() > (start + relativedelta(seconds = waitTimeoutSeconds)):
-			latestVideoUrl = None
-			elapsedTime = "N/A"
-			break
-		else:
-			# Loop. Wait a second so that we're not spamming the API too quickly.
-			sleep(1)
+    # Get a list of videos, assume page 0 has the most recent. Convert the JSON response to a dict.
+    responseData = get("https://rest-prde.immedia-semi.com/api/v2/videos/page/0", headers = {"Host":"prod.immedia-semi.com", "TOKEN_AUTH":token}).json()
+    
+    # Loop through all the videos to find the most recent
+    latestVideoTimestamp = datetime.utcfromtimestamp(0)
+    for video in responseData:
+        videoTimestamp = datetime.strptime(video["created_at"], "%Y-%m-%dT%H:%M:%S+00:00") + relativedelta(hours = timeOffsetHours)
+        
+        if videoTimestamp > latestVideoTimestamp:
+            latestVideoTimestamp = videoTimestamp
+            latestVideoUrl = video["address"]
+    
+    # Check to see if it's recent.
+    if latestVideoTimestamp > (start - relativedelta(minutes = goBackMinutes)):
+        # It's recent, continue.
+        elapsedTime = abs(datetime.today() - start).total_seconds()
+        break
+    else:
+        # It's old.
+        # If we've been looping longer than the waitTimeoutSeconds, break.
+        if datetime.today() > (start + relativedelta(seconds = waitTimeoutSeconds)):
+            latestVideoUrl = None
+            elapsedTime = "N/A"
+            break
+        else:
+            # Loop. Wait a second so that we're not spamming the API too quickly.
+            sleep(1)
 
 # If we have a video send it.
 # Message currently includes an elapsedTime for debugging.
 if latestVideoUrl != None:
-	# Download the video.
-	response = get("https://rest-prde.immedia-semi.com{0}".format(latestVideoUrl), headers = {"Host":"prod.immedia-semi.com", "TOKEN_AUTH":token})
+    # Download the video.
+    response = get("https://rest-prde.immedia-semi.com{0}".format(latestVideoUrl), headers = {"Host":"prod.immedia-semi.com", "TOKEN_AUTH":token})
 
-	# Save it to disk.
-	f = open("{0}/BlinkVideo.mp4".format(videoSavePath), 'wb')
-	f.write(response.content)
-	f.close()
-	
-	# Send the iOS notification
-	headers = {
-		'content-type':'application/json',
-		'X-HA-Access':secret["blinkHassApiPassword"]
-	}
-	
-	data = {
-		"message": "Doorbell! ({0}s)".format(elapsedTime),
-		"data": {
-			"attachment": {
-				"url": "{0}/local/BlinkVideo.mp4".format(secret["blinkHassApiBaseURL"]),
-				"content-type": "mp4"
-			}
-		}
-	}
-	
-	for entityName in notifyEntityNames:
-		post('{0}/api/services/notify/{1}'.format(secret["blinkHassApiBaseURL"], entityName), headers = headers, data = json_dumps(data))
+    # Save it to disk.
+    f = open("{0}/BlinkVideo.mp4".format(videoSavePath), 'wb')
+    f.write(response.content)
+    f.close()
+    
+    # Extract a still image from the frame at 4s and save that to disk
+    system("ffmpeg -y -ss 00:00:04 -i {0}/BlinkVideo.mp4 -vf 'crop=720:720:280:0' -vframes 1 -vcodec png {0}/BlinkImage.png".format(videoSavePath))
+    
+    # Send the iOS notification
+    headers = {
+        'content-type':'application/json',
+        'X-HA-Access':secret["blinkHassApiPassword"]
+    }
+    
+    data = {
+        "message": "Doorbell! ({0}s)".format(elapsedTime),
+        "data": {
+            "attachment": {
+                "url": "{0}/local/BlinkVideo.mp4?1=1".format(secret["blinkHassApiBaseURL"]),
+                "content-type": "mp4"
+            }
+        }
+    }
+    
+    for entityName in notifyEntityNames:
+        post('{0}/api/services/notify/{1}'.format(secret["blinkHassApiBaseURL"], entityName), headers = headers, data = json_dumps(data))
 
 # If we don't have a video, just send a message.
 # Message currently includes an elapsedTime for debugging.
 else:
-	headers = {
-		'content-type':'application/json',
-		'X-HA-Access':secret["blinkHassApiPassword"]
-	}
-	
-	data = {
-		"message": "Doorbell! ({0}s)".format(elapsedTime)
-	}
-	
-	for entityName in notifyEntityNames:
-		post('{0}/api/services/notify/{1}'.format(secret["blinkHassApiBaseURL"], entityName), headers = headers, data = json_dumps(data))
+    headers = {
+        'content-type':'application/json',
+        'X-HA-Access':secret["blinkHassApiPassword"]
+    }
+    
+    data = {
+        "message": "Doorbell! ({0}s)".format(elapsedTime)
+    }
+    
+    for entityName in notifyEntityNames:
+        post('{0}/api/services/notify/{1}'.format(secret["blinkHassApiBaseURL"], entityName), headers = headers, data = json_dumps(data))
